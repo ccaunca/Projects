457dd502b93c8eb3ed00950968517a8553ac1b12
ccaunca <ccaunca@gmail.com>
2017-03-02 17:39:33 -0800

    Added CategoryEnum

---
+++ Applications/Budget/Budget/Enum/CategoryEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.Enum
+{
+    public enum CategoryEnum
+    {
+        Groceries = 1,
+        DiningOut,
+        Mortgage,
+        Adventure,
+        Gas,
+        Gifts,
+        Household,
+        Shopping
+    }
+}
